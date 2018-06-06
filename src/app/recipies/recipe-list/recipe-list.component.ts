09e1d6b7f20e45b9adf2a149091e30d20bec3042
JaganChary <jagannadhachary7182@gmail.com>
2018-06-06 11:02:07 +0530

    added a recipe and displayed it, added structure to recipe and created array to store it, minor stying changes

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipies.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipies: Recipe[] = [
+    new Recipe ('Test Recipe', 'This is a test recipe', 'https://thumbs.dreamstime.com/z/pepperoni-pizza-thinly-sliced-popular-topping-american-style-pizzerias-30402134.jpg')
+  ];
 
   constructor() { }
 
