21252b22481ca988c40e50e59e1c5ee9cc7bcdce
JaganChary <jagannadhachary7182@gmail.com>
2018-06-06 12:04:22 +0530

    creaed shopping list section, edit, delete and add items, creted structure for ingredients and displaying them, added details about every recipe

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+    new Ingredients('Oranges', 5),
+    new Ingredients('Tomatoes', 10),
+    new Ingredients('Carrots', 7)
+  ];
 
   constructor() { }
 
